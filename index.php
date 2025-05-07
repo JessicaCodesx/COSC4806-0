6503ac9b1c21aba885273381142e23c09a79c743
Jessica Garcia <139712565+JessicaCodesx@users.noreply.github.com>
2025-05-07 00:35:45 +0000

    added sign in check to index.php with redirect to login page if user isn't signed in

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php
+// start session to access session vars
+session_start();
+
+;// check if the user is logged in
+if(!isset($_SESSION['user'])) {
+  // if not logged in redirect to login page
+  header('Location: login.php');
+  exit();
+}
+?>
+
 <html>
   <head>
     <title>Welcome</title>
